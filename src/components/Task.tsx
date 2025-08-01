a6cdf54ee6ae1c45204a4be28992549f1654b690
THanyaTHornnn <woonnasena49@gmail.com>
2025-08-01 17:07:13 +0700

    completed

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,14 +1,14 @@
 import { type  TaskCardProps } from "../libs/types";
-export function Task({title, description}: TaskCardProps) {
+export function Task(props: TaskCardProps) {
     return (
         <div className="card mb-3">
       <div className="card-body">
         <div className="row">
           <div className="col-lg-4">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{props.title}</h5>
           </div>
           <div className="col-lg-4">
-            <p className="card-text">{description}</p>
+            <p className="card-text">{props.description}</p>
           </div>
           <div className="col-lg-2">
             <button className="btn btn-success me-2">Done</button>
